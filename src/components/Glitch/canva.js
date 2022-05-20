b303b0dfb7f9c9f5f08a94b3c152a918319b22d5
Sullivan Wilquin <Skullyfox.yt@gmail.com>
2022-05-20 13:52:00 +0200

    Resolve UI issue

--- src/components/Glitch/canva.js
+++ src/components/Glitch/canva.js
@@ -1,8 +1,16 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 
 const LazyGlitch = lazy(() => import('./glitch'));
-const isBrowser = () => typeof window !== 'undefined';
+//const isBrowser = () => typeof window !== 'undefined';
 
-const Canvas =  () => isBrowser() ? <LazyGlitch /> : <canvas />
+//const Canvas =  () => isBrowser() ? <LazyGlitch /> : <canvas />
+
+const Canvas = () => {
+ return(
+   <Suspense fallback={<div>Chargement...</div>}>
+    <LazyGlitch />
+   </Suspense>
+ )
+}
 
 export default Canvas
